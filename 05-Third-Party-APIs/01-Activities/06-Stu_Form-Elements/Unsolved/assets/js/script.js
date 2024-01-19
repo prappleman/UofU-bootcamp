a607954804622c6a6ef82df00559c913878d3520
parkerrappleye <parker.rappleye1@gmail.com>
2024-01-18 21:27:50 -0700

    day 1 third party APIs

--- 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,19 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+var inputE1 = $('input[name="shopping-input"]')
 
+
+var addItem = $('.btn btn-info');
+var listItems = [];
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function updateList(event) {
+    event.preventDefault();
+    var newItem = $('<li>');
+    newItem.text(inputE1.val());
+    shoppingListEl.append(newItem);
+    $('input[type="text"]').val('');
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', updateList);
+
